159d26739eef376afc601275f01fd4f96636d71e
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2021-03-10 13:35:27 -0300

    Initial commit

---
+++ Urna Eletronica/etapas.js
@@ -0,0 +1,59 @@
+let etapas=[
+
+    // candidados a vereador
+    {
+        titulo: 'VEREADOR',
+        numeros:5,
+        candidatos: [
+           {
+               numero: '38111',
+               name: 'Fulano de tal',
+               partido: 'ABC',
+               fotos: [
+                   {url: 'foto-1-2.jpg', legenda: 'Vereador', small: false}
+               ]
+           },
+
+           { 
+               numero: '77222',
+               name: 'Beltrano da Silva',
+               partido: 'DEFG',
+               fotos: [
+                   {url: 'foto-1.jpg', legenda: 'Vereador'}
+               ]
+
+           },
+           
+        ]
+    },
+    
+  // candidatos a prefeito
+    {
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos: [
+            {
+                numero: '99',
+                name:'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                     {url:'99.jpg', legenda: 'Prefeito'},
+                     {url: '99_2_jpg', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                name: 'Zulano',
+                partido: 'QWERTY',
+                vice:'Zul', 
+                fotos: [
+                    {url: '84_jpg', legenda: 'Prefeito'},
+                    {url: '84-2.jpg', legenda: 'Vice-Prefeito', samll: true}
+                ]
+            },    
+
+        ]
+    }
+
+];
---
+++ Urna Eletronica/script.js
@@ -0,0 +1,112 @@
+let seuVotoPara = document.querySelector('.d-1-1 span'); 
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral =document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+
+let etapaAtual = 0;
+let numero = '';
+let votoBranco = true;
+
+
+const comecarEtapa = () =>{
+    let etapa = etapas[etapaAtual];
+    let numeroHtml = ' ';
+    votoBranco =false;
+    numeros = document.querySelector('.d-1-3');
+   
+    for(let i = 0; i< etapa.numeros; i++){
+      
+       if( i == 0) {
+        numeroHtml += '<div class="numero pisca"></div>'
+
+       }else {
+           numeroHtml += '<div class="numero"></div>'
+       }
+       
+    }
+
+    seuVotoPara.style.display = "none";
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML ='';
+    numeros.innerHTML = numeroHtml;
+
+}
+
+function atualizaInterface(){
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item) =>{
+        if(item.numero == numeros.substr(23, 5)){
+            return true;
+        }else{
+            return false;
+        }
+    });
+    
+    if(candidato.length > 0){
+        candidato =candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display='block';
+        descricao.innerHTML = `Nome: ${candidato.name} </br> Partido: ${candidato.partido}` ;
+
+        //
+
+        let fotosHtml = '';
+
+        for(let i in candidato.fotos){
+            fotosHtml += `<div class="d-1-image"><img src="imagem/${candidato.fotos[i].url}" alt=""> ${candidato.fotos[i].legenda}</div>`
+        }
+        lateral.innerHTML = fotosHtml;
+    }else {
+        seuVotoPara.style.display ="block";
+        aviso.style.display= 'black';
+        descricao.innerHTML = `<div class="aviso--grande pisca">VOTO NULO</div>`
+    }
+}
+ 
+const clicou = (n)=>{
+     let elNumero = document.querySelector('.numero.pisca');
+     if(elNumero !== null){
+        elNumero.innerHTML = n;
+        numeros = `${numeros}${n}`;
+        
+        //retira a classe 'pisca' da div ;
+        elNumero.classList.remove('pisca');
+
+        elNumero.nextElementSibling;
+        if(elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca');
+        }else{
+            atualizaInterface();
+        }
+     }
+ } 
+
+const branco =() =>{
+    if (numero == ''){
+        votoBranco =true;
+        seuVotoPara.style.display = 'block';
+        aviso.style.display ='block';
+        numeros.innerHTML = '<div class="aviso--grande">VOTO EM BRANCO</div>';
+    }
+    
+}
+
+const corrige =() =>{
+    comecarEtapa();
+}
+
+const confirma =() =>{
+    alert('clicou em confirma');
+}
+
+comecarEtapa();
+
+
+
+
+
